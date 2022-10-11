9c11b34dc44c0da83d41877702499c3ff24baf3c
Nickolas Orellana <p23norellanagonzalez@fairfieldprep.org>
2022-10-11 10:47:10 -0400

    done

---
+++ Lab_4-1.py
@@ -0,0 +1,11 @@
+# author NDO 10/11/22
+
+# prompts user to enter his first name 
+first_name = input('Enter your first name here')
+
+# prompts suer to enter his last name 
+last_name = input("Enter your last name here")
+
+# displays the user's full name as a result of the addition of an empty string that solves the spacing between both names
+fulll_name = first_name + '' + last_name
+print(fulll_name)
